04dd3640dcb9aa5ae78a7b084ed1ae68c7ece32e
Erick Castille <ecastille924@gmail.com>
2021-07-20 19:58:13 -0400

    build out constructors for lyrics and artists

---
+++ frontend/src/artist.js
@@ -0,0 +1,5 @@
+class Artist{
+    constructor(artistName){
+        this.artistName = artistName;
+    }
+}
---
+++ frontend/src/lyric.js
@@ -0,0 +1,11 @@
+class Lyric{
+    constructor(content, songName, albumName, releaseYear, genre, artist_id){
+        this.content = content;
+        this.songName = songName;
+        this.albumName = albumName;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+        this.artist_id = artist_id;
+    }
+    
+}
